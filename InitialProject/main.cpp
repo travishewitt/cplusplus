80eff88bdfb5d84a4b9f06ec52a3958fddf58321
Travis Hewitt <travis.j.hewitt@gmail.com>
2019-08-03 21:02:08 -0600

    Initial Commit

---
+++ InitialProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InitialProject
+//
+//  Created by Travis Hewitt on 8/3/19.
+//  Copyright © 2019 Travis Hewitt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
