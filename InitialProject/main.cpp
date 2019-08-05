45f4049dea25ab26448062792c484e122cf31634
Travis Hewitt <travis.j.hewitt@gmail.com>
2019-08-04 21:02:01 -0600

    hi

--- InitialProject/main.cpp
+++ InitialProject/main.cpp
@@ -3,13 +3,19 @@
 //  InitialProject
 //
 //  Created by Travis Hewitt on 8/3/19.
-//  Copyright © 2019 Travis Hewitt. All rights reserved.
+//  Counting Calories Program
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    //variable declarations and initializations below
+    int carb_grams = 30;
+    int protein_grams = 5;
+    int fat_grams = 15;
+    //below is a declaration, intialization, and a computation
+    int calories = calories = 4 * carb_grams + 4 * protein_grams + 9 * fat_grams;
+    //output below
+    cout << "There are " << calories << " calories in this dish." << endl;
 }
